309439e31e0ab86e9f5a0b1a896211bd7dd550df
Sagnik Chakraborty <sagnik.chakraborty910@yahoo.com>
2022-01-26 17:31:15 +0530

    dashboard page layout complete

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,32 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
-// import LandingPage from "./pages/Landing";
-import { Landing, Error, Register, Dashboard } from "./pages/index";
+import { Landing, Error, Register, ProtectedRoute } from "./pages/index";
+import {
+  AddJob,
+  AllJobs,
+  Profile,
+  Stats,
+  SharedLayout,
+} from "./pages/dashboard/index";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <SharedLayout />
+            </ProtectedRoute>
+          }
+        >
+          <Route index element={<Stats />} />
+          <Route path="all-jobs" element={<AllJobs />} />
+          <Route path="add-job" element={<AddJob />} />
+          <Route path="profile" element={<Profile />} />
+        </Route>
+
         <Route path="/register" element={<Register />} />
         <Route path="/landing" element={<Landing />} />
         <Route path="*" element={<Error />} />
