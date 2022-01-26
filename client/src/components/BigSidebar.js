00e312b71535e33cc598a6da9ad893549991d829
Sagnik004 <sagnikchakraborty910@yahoo.com>
2022-01-27 02:40:54 +0530

    big sidebar complete

--- client/src/components/BigSidebar.js
+++ client/src/components/BigSidebar.js
@@ -1,9 +1,25 @@
+import { useAppContext } from "../context/appContext";
 import Wrapper from "../assets/wrappers/BigSidebar";
+import Logo from "./Logo";
+import NavLinks from "./NavLinks";
 
 const BigSidebar = () => {
+  const { showSidebar } = useAppContext();
+
   return (
     <Wrapper>
-      <h4>Big Sidebar</h4>
+      <div
+        className={
+          showSidebar ? "sidebar-container" : "sidebar-container show-sidebar"
+        }
+      >
+        <div className="content">
+          <header>
+            <Logo />
+          </header>
+          <NavLinks />
+        </div>
+      </div>
     </Wrapper>
   );
 };
--- client/src/components/SmallSidebar.js
+++ client/src/components/SmallSidebar.js
@@ -1,10 +1,9 @@
 import { FaTimes } from "react-icons/fa";
-import { NavLink } from "react-router-dom";
 
 import Wrapper from "../assets/wrappers/SmallSidebar";
 import { useAppContext } from "../context/appContext";
-import links from "../utils/links";
 import Logo from "./Logo";
+import NavLinks from "./NavLinks";
 
 const SmallSidebar = () => {
   const { showSidebar, toggleSidebar } = useAppContext();
@@ -21,25 +20,7 @@ const SmallSidebar = () => {
             <Logo />
           </header>
 
-          <div className="nav-links">
-            {links.map((link) => {
-              const { text, path, id, icon } = link;
-
-              return (
-                <NavLink
-                  to={path}
-                  className={({ isActive }) =>
-                    isActive ? "nav-link active" : "nav-link"
-                  }
-                  key={id}
-                  onClick={toggleSidebar}
-                >
-                  <span className="icon">{icon}</span>
-                  {text}
-                </NavLink>
-              );
-            })}
-          </div>
+          <NavLinks handleNavLinkClick={toggleSidebar} />
         </div>
       </div>
     </Wrapper>
