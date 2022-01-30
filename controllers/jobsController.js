8bce7e876dbd00a188bd4cdcff1061b52945603c
Sagnik004 <sagnikchakraborty910@yahoo.com>
2022-01-30 20:47:48 +0530

    job controller complete

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -1,5 +1,26 @@
+import { StatusCodes } from "http-status-codes";
+
+import Job from "../models/Job.js";
+import { BadRequestError, NotFoundError } from "../errors/index.js";
+
 const createJob = async (req, res) => {
-  res.send("createJob function");
+  const { position, company, status, jobType, jobLocation } = req.body;
+
+  if (!position || !company) {
+    throw new BadRequestError("Please provide all values");
+  }
+
+  const userJobDetails = {
+    company,
+    position,
+    status,
+    jobType,
+    jobLocation,
+    createdBy: req.user.userId,
+  };
+  const newJob = await Job.create(userJobDetails);
+
+  res.status(StatusCodes.CREATED).json({ job: newJob });
 };
 
 const getAllJobs = async (req, res) => {
