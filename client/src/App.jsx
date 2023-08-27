2d653ab9909dc9408fce215d7514cacae9a5c436
Sagnik Chakraborty <sagnik.chakraborty910@yahoo.com>
2023-08-28 00:24:31 +0530

    createBrowserRouter & RouterProvider added to App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,18 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <h1>Home page</h1>,
+  },
+  {
+    path: '/about',
+    element: <h1>About page</h1>,
+  },
+]);
+
 const App = () => {
-  return (
-    <div>Hello there!</div>
-  );
-}
+  return <RouterProvider router={router} />;
+};
 
 export default App;
