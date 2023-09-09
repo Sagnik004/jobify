dc6fc9c7e423ce366b832f268bf1fdbf713649c9
Sagnik Chakraborty <sagnik.chakraborty910@yahoo.com>
2023-09-10 00:56:29 +0530

    error page for 404 complete

--- client/src/pages/Error.jsx
+++ client/src/pages/Error.jsx
@@ -1,14 +1,31 @@
 import { Link, useRouteError } from "react-router-dom";
 
+import Wrapper from "../assets/wrappers/ErrorPage";
+import notFoundImg from '../assets/images/not-found.svg';
+
 const Error = () => {
   const error = useRouteError();
   console.log(error);
+
+  if (error.status === 404) {
+    return (
+      <Wrapper>
+        <div>
+          <img src={notFoundImg} alt="404 not found" />
+          <h3>Ohh! Page not found.</h3>
+          <p>We can&apos;t seem to find the page you&apos;re looking for!</p>
+          <Link to='/dashboard'>Back home</Link>
+        </div>
+      </Wrapper>
+    );
+  }
   
   return (
-    <div>
-      <h1>Error Page</h1>
-      <Link to='/'>Back to home</Link>
-    </div>
+    <Wrapper>
+      <div>
+        <h3>Something went wrong</h3>
+      </div>
+    </Wrapper>
   );
 }
 
