6f988e3e124ad2f747431a9ed00a120b1323251a
Sagnik Chakraborty <sagnik.chakraborty910@yahoo.com>
2024-01-17 01:03:28 +0530

    added morgan and dotenv

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 import express from 'express';
+import morgan from 'morgan';
+import * as dotenv from 'dotenv';
 
-const port = 5100;
+dotenv.config();
 const app = express();
+const port = process.env.PORT || 5100;
 
 // Middleware's
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));  // For better development logs
+}
 app.use(express.json()); // Allow to receive JSON payloads
 
 app.get('/', (req, res) => {
@@ -15,5 +21,5 @@ app.post('/', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}...`);
+  console.log(`Server running on port:${port}...`);
 });
