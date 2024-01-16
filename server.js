6751431d33b6578349a813db5c7b7e6d57cc23ac
Sagnik Chakraborty <sagnik.chakraborty910@yahoo.com>
2024-01-17 01:33:50 +0530

    get_all_jobs dummy route added

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 import express from 'express';
 import morgan from 'morgan';
 import * as dotenv from 'dotenv';
+import { nanoid } from 'nanoid';
+
+// Local data (temporary)
+let jobs = [
+  {id: nanoid(), company: 'apple', position: 'front-end'},
+  {id: nanoid(), company: 'google', position: 'back-end'},
+];
 
 dotenv.config();
 const app = express();
@@ -12,12 +19,16 @@ if (process.env.NODE_ENV === 'development') {
 }
 app.use(express.json()); // Allow to receive JSON payloads
 
+// Routes
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
 app.post('/', (req, res) => {
   console.log(req);
   res.json({ message: 'data received', data: req.body });
+});
+app.get('/api/v1/jobs', (req, res) => {
+  res.status(200).json({ jobs })
 });
 
 app.listen(port, () => {
