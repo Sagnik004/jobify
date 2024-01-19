2520e82643595dab9e165475a14f07ed6dcd5b94
Sagnik Chakraborty <sagnik.chakraborty910@yahoo.com>
2024-01-20 01:40:10 +0530

    connected create_job to db

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -1,5 +1,7 @@
 import { nanoid } from 'nanoid';
 
+import Job from '../models/JobModel.js';
+
 // Local data (temporary)
 let jobs = [
   { id: nanoid(), company: 'apple', position: 'front-end' },
@@ -7,13 +9,7 @@ let jobs = [
 ];
 
 export const createNewJob = async (req, res) => {
-  const { company, position } = req.body;
-  if (!company || !position) {
-    return res.status(400).json({ msg: 'please provide company and position' });
-  }
-  const id = nanoid(10);
-  const job = { id, company, position };
-  jobs.push(job);
+  const job = await Job.create(req.body);
   res.status(201).json({ job });
 }
 
