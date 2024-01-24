6cf1df49aaa3b52be011c849051b08ede9cedf9f
Sagnik Chakraborty <sagnik.chakraborty910@yahoo.com>
2024-01-25 02:31:16 +0530

    added admin user logic

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,6 +3,10 @@ import { StatusCodes } from 'http-status-codes';
 import User from '../models/UserModel.js';
 
 export const register = async (req, res) => {
+  // First user to register in the site will be an admin
+  const isFirstAccount = (await User.countDocuments()) === 0;
+  req.body.role = isFirstAccount ? 'admin' : 'user';
+
   const user = await User.create(req.body);
   res.status(StatusCodes.CREATED).json({ user });
 };
