4bdcd8668f10bed21c2162af93c1d3be86916652
Sagnik Chakraborty <sagnik.chakraborty910@yahoo.com>
2024-02-04 01:39:22 +0530

    create JWT during login and pass back as HTTP only cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,12 +3,13 @@ import { StatusCodes } from 'http-status-codes';
 import User from '../models/UserModel.js';
 import { comparePassword, hashPassword } from '../utils/passwordUtils.js';
 import { UnauthenticatedError } from '../errors/customErrors.js';
+import { createJWT } from '../utils/tokenUtils.js';
 
 export const register = async (req, res) => {
   // First user to register in the site will be an admin
   const isFirstAccount = (await User.countDocuments()) === 0;
   req.body.role = isFirstAccount ? 'admin' : 'user';
-  
+
   // Hash password
   const hashedPassword = await hashPassword(req.body.password);
   req.body.password = hashedPassword;
@@ -34,5 +35,14 @@ export const login = async (req, res) => {
   // const isValidUser = user && (await comparePassword(password, user.password));
   // if (!isValidUser) throw new UnauthenticatedError('Invalid credentials');
 
-  res.send('login page');
+  // Generate JWT and send back as HTTP Only cookie which should expire in 1 day
+  const token = createJWT({ userId: user._id, role: user.role });
+  const oneDayInMillis = 24 * 60 * 60 * 1000;
+  res.cookie('token', token, {
+    httpOnly: true,
+    expires: new Date(Date.now() + oneDayInMillis),
+    secure: process.env.NODE_ENV === 'production',
+  });
+
+  res.status(StatusCodes.OK).json({ msg: 'User logged in' });
 };
