40ad0beb952b8bc481d0ab4552057186945d8520
Sagnik Chakraborty <sagnik.chakraborty910@yahoo.com>
2024-02-04 02:30:52 +0530

    added createdBy user to jobs and get_all_jobs return specific to logged in user

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -3,12 +3,13 @@ import { StatusCodes } from 'http-status-codes';
 import Job from '../models/JobModel.js';
 
 export const createNewJob = async (req, res) => {
+  req.body.createdBy = req.user.userId;
   const job = await Job.create(req.body);
   res.status(StatusCodes.CREATED).json({ job });
 };
 
 export const getAllJobs = async (req, res) => {
-  const jobs = await Job.find({});
+  const jobs = await Job.find({ createdBy: req.user.userId });
   res.status(StatusCodes.OK).json({ jobs });
 };
 
